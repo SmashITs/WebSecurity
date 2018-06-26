91d2aec17b772649c45de81ef7a44eb36afac24b
SmashITs <31624228+SmashITs@users.noreply.github.com>
2018-06-26 12:25:10 +0200

    Update ex1.php

--- ex1.php
+++ ex1.php
@@ -3,10 +3,16 @@
 <head><title>Ex 1: Bypass htmlspecialchars in Javascript context.</title></head>
 <body>
 <script>
+var check = 0;
 function solution() {
     var x = document.getElementById("myDIV");
     if (x.style.display === "none") {
-		x.innerHTML = atob("U29sdXRpb246IDthbGVydCgzKTs=");
+		if(check == 0){
+			var unescaped_text = atob("U29sdXRpb246IDthbGVydCgzKTs=");
+			var text_node = document.createTextNode(unescaped_text);
+			x.appendChild(text_node);
+			check = check + 1;
+		}
         x.style.display = "block";
     } else {
         x.style.display = "none";
@@ -14,16 +20,17 @@ function solution() {
 } 
 </script>
 <?php
-//The only interesting stuff is in the PHP part.
+//Begin Hackit:****************************************************************
 if(isset($_GET['val']) && !empty($_GET['val']) && !is_array($_GET['val'])){
-$xp = htmlspecialchars($_GET['val'],ENT_QUOTES,'UTF-8');
-echo "Your input was: $xp";
-echo '<script>
-function test(){
-	foo = "Are you in a js context with this input?\n=>" + ' .$xp.';
-	alert(foo);
-}
-</script>';
+	$xp = htmlspecialchars($_GET['val'],ENT_QUOTES,'UTF-8');
+	echo "Your input was: $xp";
+	echo '<script>
+	function test(){
+		foo = "Are you in a js context with this input?\n=>" + ' .$xp.';
+		alert(foo);
+	}
+	</script>';
+//End Hackit******************************************************************
 }
 else{
 	$actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
